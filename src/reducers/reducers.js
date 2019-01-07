bf8f71d4377deceef254433816bae9c00097d70c
Guilherme Gutierres Gallo <guilherme.gg1@hotmail.com>
2019-01-07 02:28:07 -0200

    users view fixed

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -2,17 +2,43 @@ import { List } from 'immutable';
 
 function reducer(name, state, action) {
   switch (action.type) {
+    case 'LIST_USERS':
+      let newList = action.list.map(user => {
+        let newUser = { permFriendly: permissionsFriendly(user.permissions) };
+        for(let propertyName in user)
+          newUser[propertyName] = user[propertyName];
+
+        return newUser;
+      });
+      return { list: new List(newList), key: action.key }
+
     case `LIST_${name}`:
-      return {
-        list: new List(action.list),
-        order: 'none',
-        orderBy: 'none',
-        key: action.key
-      }
+      return { list: new List(action.list), key: action.key }
 
     default:
       return state;
   }
+}
+
+const permissionsFriendly = (permissions) => {
+  let arrFriendly = {};
+  permissions.forEach(permission => {
+    const divider = permission.indexOf(':');
+    const route = permission.substr(0, divider);
+    const level = permission.substr(divider + 1, 1).toUpperCase();
+
+    if(arrFriendly.hasOwnProperty(route)) {
+      arrFriendly[route] += level;
+    } else {
+      arrFriendly[route] = level;
+    }
+  });
+
+  let str = '';
+  for(let propertyName in arrFriendly)
+    str += `${propertyName} ${arrFriendly[propertyName]} | `;
+
+  return str.substr(0, str.length -3);
 }
 
 export const accounts = (state = [], action) => reducer('ACCOUNTS', state, action);
