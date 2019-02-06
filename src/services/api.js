bb8231fd73d7f8988bdf67048fcad9a16cbe79e4
guigallo <guilherme.gg1@hotmail.com>
2019-02-06 14:47:16 -0200

    Log path_api

--- src/services/api.js
+++ src/services/api.js
@@ -5,6 +5,7 @@
 import configs from '../configs/configs';
 
 const PATH_API = process.env.PATH_API || configs.apiAdrress;
+console.log(PATH_API)
 
 //user create init
 export const logIn = (email, password) =>
