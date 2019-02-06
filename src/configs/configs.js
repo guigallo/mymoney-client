11d56ff953373155d824958b427474fa6ebf21a1
guigallo <guilherme.gg1@hotmail.com>
2019-02-06 15:14:30 -0200

    Api url. Removed remember me from login

--- src/configs/configs.js
+++ src/configs/configs.js
@@ -3,5 +3,6 @@ const apiHost = 'localhost';
 const apiPort = '3001';
 
 export default {
-  apiAdrress: `${apiProtocol}://${apiHost}:${apiPort}`
+  //apiAdrress: `${apiProtocol}://${apiHost}:${apiPort}`
+  apiAdrress: 'http://mymoney-server.herokuapp.com'
 }
